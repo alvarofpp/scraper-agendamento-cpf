33a25851ca8eb99652c7afcc89e17c3702731f06
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2022-02-01 18:31:26 -0300

    chore: Execute script one time per minute

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from datetime import datetime
+import logging
+from time import sleep
 
 from core import Extract, Load, Transform
 
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s | %(message)s',
+    encoding='utf-8',
+    level=logging.DEBUG,
+)
 
-# Extract TRs
-trs = Extract.get_trs()
+while True:
+    # Extract TRs
+    trs = Extract.get_trs()
 
-# Transform to CSV row
-now = datetime.now()
-rows = [Transform.convert_tr_to_row(now, tr) for tr in trs]
+    # Transform to CSV row
+    now = datetime.now()
+    rows = [Transform.convert_tr_to_row(now, tr) for tr in trs]
 
-# Load data in the CSV
-Load.add_rows_to_csv(rows)
+    # Load data in the CSV
+    Load.add_rows_to_csv(rows)
+    logging.info('New lines: {}'.format(len(rows)))
+
+    # Sleep until next minute
+    sleeptime = (60 - datetime.utcnow().second) + 1
+    sleep(sleeptime)
